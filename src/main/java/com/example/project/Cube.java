901d61d1f41e70a97988dd595fb63120928a4646
Spence <120441242+5pencer5mith@users.noreply.github.com>
2023-07-07 15:42:13 -0600

    Update Cube.java
    Finished Product without solution optimization. Cube takes in command line arguments (if they're present) or allows the user to rotate the cube if command line arguments are not present. 
    
    Program also prints a solution for the mixed cube.

--- src/main/java/com/example/project/Cube.java
+++ src/main/java/com/example/project/Cube.java
@@ -1,126 +1,444 @@
 package com.example.project;
 
-public class Cube {
-static String[][] Face = {
-            {"0", "1", "2"},
-            {"3", "4", "5"},
-            {"6", "7", "8"}
-    };
-
+import java.util.Scanner;
+import java.util.*;
+public class TheCube {
     static String[][][] theCUBE = {
-            {
-                    {"0R", "1R", "2R"},
-                    {"3R", "4R", "5R"},
-                    {"6R", "7R", "8R"}
+            { //top face [0][y][x]
+                    {" 0y ", " 1y ", " 2y "},
+                    {" 3y ", " 4y ", " 5y "},
+                    {" 6y ", " 7y ", " 8y "}
             },
-            {
-                    {"0B", "1B", "2B"},
-                    {"3B", "4B", "5B"},
-                    {"6B", "7B", "8B"}
+            { //front face [1][y][x]
+                    {" 0b ", " 1b ", " 2b "},
+                    {" 3b ", " 4b ", " 5b "},
+                    {" 6b ", " 7b ", " 8b "}
             },
-            {
-                    {"0Y", "1Y", "2Y"},
-                    {"3Y", "4Y", "5Y"},
-                    {"6Y", "7Y", "8Y"}
+            { // right face [2][y][x]
+                    {" 0r ", " 1r ", " 2r "},
+                    {" 3r ", " 4r ", " 5r "},
+                    {" 6r ", " 7r ", " 8r "}
             },
-            {
-                    {"0G", "1G", "2G"},
-                    {"3G", "4G", "5G"},
-                    {"6G", "7G", "8G"}
+            { //back face [3][y][x]
+                    {" 0g ", " 1g ", " 2g "},
+                    {" 3g ", " 4g ", " 5g "},
+                    {" 6g ", " 7g ", " 8g "}
             },
-            {
-                    {"0W", "1W", "2W"},
-                    {"3W", "4W", "5W"},
-                    {"6W", "7W", "8W"}
+            { //left face [4][y][x]
+                    {" 0o ", " 1o ", " 2o "},
+                    {" 3o ", " 4o ", " 5o "},
+                    {" 6o ", " 7o ", " 8o "}
             },
-            {
-                    {"0o", "1o", "2o"},
-                    {"3o", "4o", "5o"},
-                    {"6o", "7o", "8o"}
+            { //bottom face [5][y][x]
+                    {" 0w ", " 1w ", " 2w "},
+                    {" 3w ", " 4w ", " 5w "},
+                    {" 6w ", " 7w ", " 8w "}
             }
 
 
     };
 
-    static void showFace(String[][] face){
-
-        for(int i = 0; i < 3; i++){
+    static void printCube(String[][][] cube){
+        for(int i = 0; i < 6; i++){
             for(int j = 0; j < 3; j++){
-                System.out.print(face[i][j]);
+                for(int k = 0; k < 3; k++){
+                    System.out.print(cube[i][j][k]);
+                }
+                System.out.println();
             }
             System.out.println();
         }
     }
 
-    static void rotateFace(String[][] face, boolean clockwise){
-
-        String[][] tempFace = new String[3][3];
+    static void rotateCube(String[][][] cube, int cubeFaceIndex, boolean clockwise){
+        String[][] tempCubeFace = new String [3][3];
         for(int i = 0; i < 3; i++){
-            for(int j = 0; j < 3; j++){
-                tempFace[i][j] = face[i][j];
-            }
+            System.arraycopy(theCUBE[cubeFaceIndex][i], 0, tempCubeFace[i], 0, 3);
         }
 
-        if(!clockwise){
+        if(clockwise){
             for(int i = 0; i < 3; i++){
                 for(int j = 0; j < 3; j++){
-                    face[i][j] = tempFace[j][i];
+                    cube[cubeFaceIndex][i][j] = tempCubeFace[j][i];
                 }
             }
-
-            String[] tempRow = new String[3];
-
             for(int i = 0; i < 3; i++){
-                tempRow[i] = face[0][i];
+                System.arraycopy(cube[cubeFaceIndex][i], 0, tempCubeFace[i], 0, 3);
             }
-            face[0] = face[2];
-            face[2] = tempRow;
+            for(int i = 0; i < 3; i++){
+                cube[cubeFaceIndex][i][0] = cube[cubeFaceIndex][i][2];
+                cube[cubeFaceIndex][i][2] = tempCubeFace[i][0];
+            }
+
+            switch(cubeFaceIndex){
+                case 0:
+                    String[] tempFrontFaceRow = new String[3];
+                    System.arraycopy(cube[1][0], 0, tempFrontFaceRow, 0, 3);
+
+                    for(int i = 0; i < 3; i++){
+                        cube[1][0][i] = cube[2][0][i];
+                        cube[2][0][i] = cube[3][0][i];
+                        cube[3][0][i] = cube[4][0][i];
+                        cube[4][0][i] = tempFrontFaceRow[i];
+                    }
+
+                    break;
+
+                case 1:
+                    String[][] tempLeftFace = new String[3][3];
+                    for(int i = 0; i < 3; i++){
+                        System.arraycopy(cube[4][i], 0, tempLeftFace[i], 0, 3);
+                    }
+
+                    cube[4][0][2] = cube[5][0][0];
+                    cube[4][1][2] = cube[5][0][1];
+                    cube[4][2][2] = cube[5][0][2];
+
+                    cube[5][0][0] = cube[2][2][0];
+                    cube[5][0][1] = cube[2][1][0];
+                    cube[5][0][2] = cube[2][0][0];
+
+                    cube[2][0][0] = cube[0][2][0];
+                    cube[2][1][0] = cube[0][2][1];
+                    cube[2][2][0] = cube[0][2][2];
+
+                    cube[0][2][0] = tempLeftFace[2][2];
+                    cube[0][2][1] = tempLeftFace[1][2];
+                    cube[0][2][2] = tempLeftFace[0][2];
+
+                    break;
+
+                case 2:
+                    String[][] tempBottomFace = new String[3][3];
+                    for(int i = 0; i < 3; i++){
+                        System.arraycopy(cube[5][i], 0, tempBottomFace[i], 0, 3);
+                    }
+                    cube[5][2][2] = cube[3][0][0];
+                    cube[5][1][2] = cube[3][1][0];
+                    cube[5][0][2] = cube[3][2][0];
+
+                    cube[3][0][0] = cube[0][2][2];
+                    cube[3][1][0] = cube[0][1][2];
+                    cube[3][2][0] = cube[0][0][2];
+
+                    cube[0][0][2] = cube[1][0][2];
+                    cube[0][1][2] = cube[1][1][2];
+                    cube[0][2][2] = cube[1][2][2];
+
+                    cube[1][0][2] = tempBottomFace[0][2];
+                    cube[1][1][2] = tempBottomFace[1][2];
+                    cube[1][2][2] = tempBottomFace[2][2];
+
+                    break;
+
+                case 3:
+                    String[][] tempLeftFaceAgain = new String[3][3];
+                    for(int i = 0; i < 3; i++){
+                        System.arraycopy(cube[4][i], 0, tempLeftFaceAgain[i], 0, 3);
+                    }
+                    cube[4][0][0] = cube[0][0][2];
+                    cube[4][1][0] = cube[0][0][1];
+                    cube[4][2][0] = cube[0][0][0];
+
+                    cube[0][0][0] = cube[2][0][2];
+                    cube[0][0][1] = cube[2][1][2];
+                    cube[0][0][2] = cube[2][2][2];
+
+                    cube[2][0][2] = cube[5][2][2];
+                    cube[2][1][2] = cube[5][2][1];
+                    cube[2][2][2] = cube[5][2][0];
+
+                    cube[5][2][0] = tempLeftFaceAgain[0][0];
+                    cube[5][2][1] = tempLeftFaceAgain[1][0];
+                    cube[5][2][2] = tempLeftFaceAgain[2][0];
+
+                    break;
+
+                case 4:
+                    String[][] tempBackFace = new String[3][3];
+                    for(int i = 0; i < 3; i++){
+                        System.arraycopy(cube[3][i], 0, tempBackFace[i], 0, 3);
+                    }
+                    cube[3][0][2] = cube[5][2][0];
+                    cube[3][1][2] = cube[5][1][0];
+                    cube[3][2][2] = cube[5][0][0];
+
+                    cube[5][0][0] = cube[1][0][0] ;
+                    cube[5][1][0] = cube[1][1][0];
+                    cube[5][2][0] = cube[1][2][0];
+
+                    cube[1][0][0] = cube[0][0][0];
+                    cube[1][1][0] = cube[0][1][0];
+                    cube[1][2][0] = cube[0][2][0];
+
+                    cube[0][0][0] = tempBackFace[2][2];
+                    cube[0][1][0] = tempBackFace[1][2];
+                    cube[0][2][0] = tempBackFace[0][2];
+
+                    break;
+
+                case 5:
+                    String[][] tempRightFace = new String[3][3];
+                    for(int i = 0; i < 3; i++){
+                        System.arraycopy(cube[2][i], 0, tempRightFace[i], 0, 3);
+                    }
+                    cube[2][2][0] = cube[1][2][0];
+                    cube[2][2][1] = cube[1][2][1];
+                    cube[2][2][2] = cube[1][2][2];
+
+                    cube[1][2][0] = cube[4][2][0];
+                    cube[1][2][1] = cube[4][2][1];
+                    cube[1][2][2] = cube[4][2][2];
+
+                    cube[4][2][0] = cube[3][2][0];
+                    cube[4][2][1] = cube[3][2][1];
+                    cube[4][2][2] = cube[3][2][2];
+
+                    cube[3][2][0] = tempRightFace[2][0];
+                    cube[3][2][1] = tempRightFace[2][1];
+                    cube[3][2][2] = tempRightFace[2][2];
+
+                    break;
+            }
         }
 
         else{
             for(int i = 0; i < 3; i++){
                 for(int j = 0; j < 3; j++){
-                    face[i][j] = tempFace[j][i];
+                    cube[cubeFaceIndex][i][j] = tempCubeFace[j][i];
                 }
             }
+
+            String[] tempRow = new String[3];
             for(int i = 0; i < 3; i++){
-                for(int j = 0; j < 3; j++){
-                    tempFace[i][j] = face[i][j];
-                }
-            }
-            for(int i = 0; i < 3; i++){
-                face[i][0] = face[i][2];
-            }
-            for(int i = 0; i < 3; i++){
-                face[i][2] = tempFace[i][0];
+                tempRow[i] = cube[cubeFaceIndex][0][i];
+                cube[cubeFaceIndex][0][i] = tempCubeFace[i][2];
+                cube[cubeFaceIndex][2][i] = tempRow[i];
             }
 
+            switch(cubeFaceIndex){
+                case 0:
+                    String[] tempFrontFaceRow = new String[3];
+                    System.arraycopy(cube[1][0], 0, tempFrontFaceRow, 0, 3);
+                    for(int i = 0; i < 3; i++){
+                        cube[1][0][i] = cube[4][0][i];
+                        cube[4][0][i] = cube[3][0][i];
+                        cube[3][0][i] = cube[2][0][i];
+                        cube[2][0][i] = tempFrontFaceRow[i];
+                    }
 
+                    break;
 
+                case 1:
+                    String[][] tempTopFace = new String[3][3];
+                    for(int i = 0; i < 3; i++){
+                        System.arraycopy(cube[0][i], 0, tempTopFace[i], 0, 3);
+                    }
+                    cube[0][2][0] = cube[2][0][0];
+                    cube[0][2][1] = cube[2][1][0];
+                    cube[0][2][2] = cube[2][2][0];
 
+                    cube[2][0][0] = cube[5][0][2];
+                    cube[2][1][0] = cube[5][0][1];
+                    cube[2][2][0] = cube[5][0][0];
+
+                    cube[5][0][0] = cube[4][0][2];
+                    cube[5][0][1] = cube[4][1][2];
+                    cube[5][0][2] = cube[4][2][2];
+
+                    cube[4][0][2] = tempTopFace[2][2];
+                    cube[4][1][2] = tempTopFace[2][1];
+                    cube[4][2][2] = tempTopFace[2][0];
+
+                    break;
+
+                case 2:
+                    String[][] tempTopFaceAgain = new String[3][3];
+                    for(int i = 0; i < 3; i++){
+                        System.arraycopy(cube[0][i], 0, tempTopFaceAgain[i], 0, 3);
+                    }
+                    cube[0][0][2] = cube[3][2][0];
+                    cube[0][1][2] = cube[3][1][0];
+                    cube[0][2][2] = cube[3][0][0];
+
+                    cube[3][0][0] = cube[5][2][2];
+                    cube[3][1][0] = cube[5][1][2];
+                    cube[3][2][0] = cube[5][0][2];
+
+                    cube[5][0][2] = cube[1][0][2];
+                    cube[5][1][2] = cube[1][1][2];
+                    cube[5][2][2] = cube[1][2][2];
+
+                    cube[1][0][2] = tempTopFaceAgain[0][2];
+                    cube[1][1][2] = tempTopFaceAgain[1][2];
+                    cube[1][2][2] = tempTopFaceAgain[2][2];
+
+                    break;
+
+                case 3:
+                    String[][] tempRightFace = new String[3][3];
+                    for(int i = 0; i < 3; i++){
+                        System.arraycopy(cube[2][i], 0, tempRightFace[i], 0, 3);
+                    }
+                    cube[2][0][2] = cube[0][0][0];
+                    cube[2][1][2] = cube[0][0][1];
+                    cube[2][2][2] = cube[0][0][2];
+
+                    cube[0][0][0] = cube[4][2][0];
+                    cube[0][0][1] = cube[4][1][0];
+                    cube[0][0][2] = cube[4][0][0];
+
+                    cube[4][0][0] = cube[5][2][0];
+                    cube[4][1][0] = cube[5][2][1];
+                    cube[4][2][0] = cube[5][2][2];
+
+                    cube[5][2][0] = tempRightFace[2][2];
+                    cube[5][2][1] = tempRightFace[1][2];
+                    cube[5][2][2] = tempRightFace[0][2];
+
+                    break;
+
+                case 4:
+                    String[][] tempBackFace = new String[3][3];
+                    for(int i = 0; i < 3; i++){
+                        System.arraycopy(cube[3][i], 0, tempBackFace[i], 0, 3);
+                    }
+
+                    cube[3][0][2] = cube[0][2][0];
+                    cube[3][1][2] = cube[0][1][0];
+                    cube[3][2][2] = cube[0][0][0];
+
+                    cube[0][0][0] = cube[1][0][0];
+                    cube[0][1][0] = cube[1][1][0];
+                    cube[0][2][0] = cube[1][2][0];
+
+                    cube[1][0][0] = cube[5][0][0];
+                    cube[1][1][0] = cube[5][1][0];
+                    cube[1][2][0] = cube[5][2][0];
+
+                    cube[5][0][0] = tempBackFace[2][2];
+                    cube[5][1][0] = tempBackFace[1][2];
+                    cube[5][2][0] = tempBackFace[0][2];
+
+                    break;
+
+                case 5:
+                    String[][] tempLeftFace = new String[3][3];
+                    for(int i = 0; i < 3; i++){
+                        System.arraycopy(cube[4][i], 0, tempLeftFace[i], 0, 3);
+                    }
+                    cube[4][2][0] = cube[1][2][0];
+                    cube[4][2][1] = cube[1][2][1];
+                    cube[4][2][2] = cube[1][2][2];
+
+                    cube[1][2][0] = cube[2][2][0];
+                    cube[1][2][1] = cube[2][2][1];
+                    cube[1][2][2] = cube[2][2][2];
+
+                    cube[2][2][0] = cube[3][2][0];
+                    cube[2][2][1] = cube[3][2][1];
+                    cube[2][2][2] = cube[3][2][2];
+
+                    cube[3][2][0] = tempLeftFace[2][0];
+                    cube[3][2][1] = tempLeftFace[2][1];
+                    cube[3][2][2] = tempLeftFace[2][2];
+
+                    break;
+            }
         }
 
+    }
 
+
+
+    static void printSolution(ArrayList solutionSet){
+        System.out.print("Solution: ");
+
+        for(int i = 0; i < solutionSet.size(); i++){
+            System.out.print(solutionSet.get(i) + " ");
+        }
+        System.out.println();
+    }
+
+    static void moveItMoveIt(String command){
+        switch(command){
+            case "U": case "u":
+                rotateCube(theCUBE, 0, true);
+                break;
+            case "U'": case "u'":
+                rotateCube(theCUBE, 0, false);
+                break;
+            case "D": case "d":
+                rotateCube(theCUBE, 5, true);
+                break;
+            case "D'": case "d'":
+                rotateCube(theCUBE, 5, false);
+                break;
+            case "R": case "r":
+                rotateCube(theCUBE, 2, true);
+                break;
+            case "R'": case "r'":
+                rotateCube(theCUBE, 2, false);
+                break;
+            case "L": case "l":
+                rotateCube(theCUBE, 4, true);
+                break;
+            case "L'": case "l'":
+                rotateCube(theCUBE, 4, false);
+                break;
+            case "F": case "f":
+                rotateCube(theCUBE, 1, true);
+                break;
+            case "F'": case "f'":
+                rotateCube(theCUBE, 1, false);
+                break;
+            case "B": case "b":
+                rotateCube(theCUBE, 3, true);
+                break;
+            case "B'": case "b'":
+                rotateCube(theCUBE, 3, false);
+                break;
+            case "X": case "x":
+                System.exit(0);
+        }
+    }
+
+    static String primeConverter(String move){
+        if(move.length() > 1){
+            return move.substring(0, 1);
+        }
+        else{
+            return move + "'";
+        }
 
     }
     public static void main(String[] args){
 
-        showFace(Face);
-        System.out.println();
+        ArrayList<String> solutionList = new ArrayList<>();
 
-        rotateFace(Face, true);
-        System.out.println();
+        if(args.length > 0){
 
-        showFace(Face);
+            for (String arg : args) {
+                moveItMoveIt(arg);
+                solutionList.add(0, primeConverter(arg));
+            }
+            printCube(theCUBE);
+            printSolution(solutionList);
 
-        rotateFace(Face, false);
-        System.out.println();
+        }
+        else{
+            Scanner userInput = new Scanner(System.in);
 
-        showFace(Face);
+            while(true){
+                String moveCube = userInput.next();
+                moveItMoveIt(moveCube);
+                solutionList.add(0, primeConverter(moveCube));
+                printCube(theCUBE);
+                printSolution(solutionList);
 
-
+            }
+        }
 
     }
-
 }
 
